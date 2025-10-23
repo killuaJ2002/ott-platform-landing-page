69d13c5eafe78db9f9a8854a67c5265173993185
killuaJ2002 <kjcodes2002@gmail.com>
2025-10-23 14:18:24 +0530

    remove black bg from suggestion card

--- src/components/SuggestionCard.jsx
+++ src/components/SuggestionCard.jsx
@@ -3,7 +3,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 const SuggestionCard = ({ title, image }) => {
   return (
     <div className="min-w-40 2xl:min-w-48 h-full rounded-md bg-white/20 flex flex-col p-2 gap-2">
-      <div className="max-h-[45%] w-full rounded bg-cover bg-center bg-black overflow-hidden">
+      <div className="max-h-[45%] w-full rounded bg-cover bg-center overflow-hidden">
         <LazyLoadImage
           src={image}
           alt="suggestion-card-image"
